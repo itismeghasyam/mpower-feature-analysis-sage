8c70d5db58729fa13991406bedd41e8c312928d2
Pranav Anbarasu <pranavanba@gmail.com>
2024-06-12 20:33:09 +0000

    Update get_github_url() and save_to_synapse() helper functions

--- utils/helper_utils.R
+++ utils/helper_utils.R
@@ -145,11 +145,12 @@ get_github_url <- function(git_token_path,
                            git_repo,
                            script_path,
                            ...){
-  githubr::setGithubToken(readLines(git_token_path))
-  githubr::getPermlink(
-    git_repo, 
-    repositoryPath = script_path,
-    ...)
+  gh::gh("/repos/:owner_repo/commits?path=:path_to_file", 
+         owner_repo = git_repo, 
+         path_to_file = script_path,
+         .token = readLines(git_token_path))[[1]]$html_url %>% 
+    stringr::str_replace("commit", "blob") %>% 
+    file.path(script_path)
 }
 
 #' Function to save to synapse
@@ -169,8 +170,7 @@ save_to_synapse <- function(data,
   file <- File(output_filename, 
                parent =  parent,
                annotations = annotations)
-  activity <- Activity(...)
-  synStore(file, activity = activity)
+  synStore(file, ...)
   unlink(file$path)
 }
 
